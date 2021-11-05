f9e4ce7ce2037a5b3ef37692d30383b553c08014
shoom3301 <shoom3301@gmail.com>
2021-11-05 13:34:49 +0600

    Second demo extend

--- demo/secondDemo/src/app/app.module.ts
+++ demo/secondDemo/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     ReactiveFormsModule,
   ],
